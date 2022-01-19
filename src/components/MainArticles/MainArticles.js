bf0969bbe6c2f7abd5d8ed6bc978d0b4c8b6f77d
whatthefoobar <irina.mntn@gmail.com>
2022-01-19 11:07:30 +0100

    changed UI before css cleanup

--- src/components/MainArticles/MainArticles.js
+++ src/components/MainArticles/MainArticles.js
@@ -1,35 +1,52 @@
 import React from "react";
+import "./MainArticles.css";
 
 const MainArticles = ({ topArticles }) => {
   return (
-    <div>
-      {/* <h2 className="main-articles__header">Top news</h2> */}
-      <section className="main-articles__list">
-        {topArticles.map((topArticle) => {
-          // console.log(topArticle);
-          return (
-            <article className="main-article" key={topArticle.uri}>
-              {topArticle.multimedia ? (
+    <section className="main-articles__list">
+      {topArticles.map((topArticle) => {
+        console.log(topArticle);
+        return (
+          <div className="main-article" key={topArticle.uri}>
+            {topArticle.multimedia ? (
+              <div className="main-article__img">
                 <img
                   src={topArticle.multimedia[0].url}
                   alt="top news article"
                 />
-              ) : null}
-              {/* <img src={topArticle.multimedia[0].url} alt="top news article" /> */}
-              <h2>{topArticle.title}</h2>
+                <a href="">{topArticle.section}</a>
+              </div>
+            ) : null}
+
+            <div className="main-article__content">
+              <h4>{topArticle.title}</h4>
               <p>{topArticle.abstract}</p>
               <p>{topArticle.byline}</p>
-
               {topArticle.url ? (
                 <a href={topArticle.url} target="_blank" className="list__link">
                   read more
                 </a>
               ) : null}
-            </article>
-          );
-        })}
-      </section>
-    </div>
+            </div>
+          </div>
+          // <article className="main-article" key={topArticle.uri}>
+          //   {topArticle.multimedia ? (
+          //     <img src={topArticle.multimedia[0].url} alt="top news article" />
+          //   ) : null}
+          //   {/* <img src={topArticle.multimedia[0].url} alt="top news article" /> */}
+          //   <h2>{topArticle.title}</h2>
+          //   <p>{topArticle.abstract}</p>
+          //   <p>{topArticle.byline}</p>
+
+          //   {topArticle.url ? (
+          //     <a href={topArticle.url} target="_blank" className="list__link">
+          //       read more
+          //     </a>
+          //   ) : null}
+          // </article>
+        );
+      })}
+    </section>
   );
 };
 
